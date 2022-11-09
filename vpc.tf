4a8d62c4c403b4cfce9eb52a23a59777749f4b4a
Nahuel Oyhanarte <noyhanarte@gmail.com>
2022-11-08 23:19:54 -0300

    modified vpc

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,18 @@
-module "vpc_example_simple-vpc" {
-  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
 
-  name   = "${var.namespace}-${var.environment}"
+  name   = local.name
 
-  cidr                 = "${var.vpc_cidr}.0.0/16"
-  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  cidr = "${var.vpc_cidr}.0.0/16"
+  azs  = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
-  private_subnets = ["${var.vpc_cidr}.1.0/24", "${var.vpc_cidr}.2.0/24", "${var.vpc_cidr}.3.0/24"]
-  public_subnets  = ["${var.vpc_cidr}.101.0/24", "${var.vpc_cidr}.102.0/24", "${var.vpc_cidr}.103.0/24"]
+  private_subnets  = ["${var.vpc_cidr}.1.0/24", "${var.vpc_cidr}.2.0/24", "${var.vpc_cidr}.3.0/24"]
+  public_subnets   = ["${var.vpc_cidr}.101.0/24", "${var.vpc_cidr}.102.0/24", "${var.vpc_cidr}.103.0/24"]
   database_subnets =  ["${var.vpc_cidr}.201.0/24", "${var.vpc_cidr}.202.0/24", "${var.vpc_cidr}.203.0/24"]
 
   enable_nat_gateway = false
   single_nat_gateway = true
 
+  tags = local.tags
 }
